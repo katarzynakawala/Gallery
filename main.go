29998a69faf117918b8fff005d848121b6ab0d23
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-12 08:56:22 +0100

    adding homeHandler and pathHandler

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
 )
 
- func homeHandler(w http.ResponseWriter, r *http.Request) {
-	 w.Header().Set("Content-Type", "text/html; charset=utf-8")
- 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +15,20 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:kat.kawala@gmail.com\">kat.kawala@gmail.com</a>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
