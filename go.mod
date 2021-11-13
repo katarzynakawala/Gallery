6ab902570774291a59405b81dc215ed37ed4ca75
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-13 08:40:27 +0100

    installing chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/katarzynakawala/Gallery
 
 go 1.17
+
+require github.com/go-chi/chi/v5 v5.0.5 // indirect
--- main.go
+++ main.go
@@ -15,6 +15,15 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:kat.kawala@gmail.com\">kat.kawala@gmail.com</a>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+	<li><b>Is there a free version?</b> Yes we offer you a trial for 30 days.</li>
+	</ul>
+	`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -24,7 +33,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 // 	default:
 // 		http.Error(w, "Page not found", http.StatusNotFound)
 // 	}
-//}
+// }
+
 type Router struct {}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -33,15 +43,15 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)	
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
-
 func main() {
 	var router Router
-	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", router)
 }
